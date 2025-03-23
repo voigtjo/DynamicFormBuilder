b084ed957c068713ec85da2c91fd6699350f2fe9
Your Name <you@example.com>
2025-03-23 14:24:33 +0100

    new port 5001

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const apiRoutes = require('./routes'); // Correctly reference routes.js
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
